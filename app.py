e920cba2b8941dcbe49a0259df981a43010ffa2d
Joshua Hawking <joshuahawking1@gmail.com>
2020-08-20 14:33:08 +0100

    Display messages if there are no connection requests or public keys.

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def home_page():
         connection_requests = ConnectionRequest.query.filter(ConnectionRequest.key_owner == user["id"]).all()
 
         # Remove expired requests
-        connection_requests = filter(lambda request: not request.is_expired(), connection_requests)
+        connection_requests = list(filter(lambda request: not request.is_expired(), connection_requests))
 
         # Create a list of objects in the format [{"id": 3, "expiryTime": 429819111}]
         connection_requests_times = []
